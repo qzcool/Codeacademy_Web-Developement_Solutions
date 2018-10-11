6ae098d0e8f4d1bf41d7592c443ff88f9f772c53
Qiao Zhang <qiao.zhang.qz@outlook.com>
2018-10-11 11:05:27 +0800

    Improvement

---
+++ 3. Getting Started with JavaScript/Rock, Paper, or Scissors/rockPaperScissors.js
@@ -0,0 +1,60 @@
+const getUserChoice = userInput => {
+  userInput = userInput.toLowerCase();
+  switch (userInput) {
+    case 'rock' || 'paper' || 'scissors':
+      return userInput;
+    default:
+      console.log('Error userInput.');
+  }
+}
+
+const getComputerChoice = () => {
+  let randomNumber = Math.floor(Math.random()*3)
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+    default:
+      console.log('Error computer choice, which is impossible.');
+  }
+}
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice===computerChoice) {
+    return 'The game is a tie!';
+  }
+  if (userChoice==='rock') {
+    if (computerChoice==='paper') {
+      return 'Computer won.';
+    } else {
+      return 'User won.';
+    }
+  }
+  if (userChoice==='paper') {
+    if (computerChoice==='scissors') {
+      return 'Computer won.';
+    } else {
+      return 'User won.';
+    }
+  }
+  if (userChoice==='scissors') {
+    if (computerChoice==='rock') {
+      return 'Computer won.';
+    } else {
+      return 'User won.';
+    }
+  }
+}
+
+const playGame = () => {
+  let userChoice = getUserChoice('rock');
+  console.log(userChoice);
+  let computerChoice = getComputerChoice();
+  console.log(computerChoice);
+  console.log(determineWinner(userChoice,computerChoice))
+}
+
+playGame()
