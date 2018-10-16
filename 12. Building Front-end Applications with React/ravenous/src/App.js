a4ef97d9f22e08974b61ee5023e5aeae5be27925
Qiao Zhang <qiao.zhang.qz@gmail.com>
2018-10-16 23:48:36 +0800

    Ravenous

---
+++ 12. Building Front-end Applications with React/ravenous/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import BusinessList from './components/BusinessList/BusinessList.js'
+import SearchBar from './components/SearchBar/SearchBar.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
+}
+
+export default App;
